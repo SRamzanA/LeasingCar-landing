29c8047b6a3af27f5fba5ad8f0feef06aec83b99
SRamzanA <ramzansmajov14@gmail.com>
2024-11-17 12:17:56 +0700

    new commit

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+
+const popup = document.getElementById("popup")
+const popupButton1 = document.getElementById("popup-button1")
+const popupButton2 = document.getElementById("popup-button2")
+const popupButton3 = document.getElementById("popup-button3")
+const popupCloseBtn = document.getElementById("popup-closeButton")
+
+function openPopup(event) {
+    event.preventDefault()
+    popup.classList.toggle("popup_not-active")
+}
+
+popupButton1.addEventListener("click", openPopup)
+popupButton2.addEventListener("click", openPopup)
+popupButton3.addEventListener("click", openPopup)
+popupCloseBtn.addEventListener("click", openPopup)
+
+
---
+++ js/script.js
@@ -0,0 +1,36 @@
+const inputRange1 = document.getElementById('inputRange1')
+const inputRange2 = document.getElementById('inputRange2')
+const inputRange3 = document.getElementById('inputRange3')
+
+const rangeValue1 = document.getElementById('rangeValue1')
+const rangeValue2 = document.getElementById('rangeValue2')
+const rangeValue3 = document.getElementById('rangeValue3')
+
+const priceSumm = document.getElementById('priceSumm')
+const resultPaument = document.getElementById('resultPaument')
+const procent = document.getElementById('procent')
+
+function updateValues() {
+    const carPrice = parseInt(inputRange1.value)
+    const initialPayment = parseInt(inputRange2.value)
+    const leaseTerm = parseInt(inputRange3.value)
+
+    rangeValue1.innerHTML = carPrice
+    rangeValue2.innerHTML = initialPayment
+    rangeValue3.innerHTML = leaseTerm
+
+    const totalLeaseSum = Math.floor(carPrice / 100 * 135)
+    const monthlyPayment = Math.floor((totalLeaseSum - initialPayment) / leaseTerm)
+
+    priceSumm.innerHTML = totalLeaseSum
+    resultPaument.innerHTML = monthlyPayment
+    procent.innerHTML = Math.floor(initialPayment / (totalLeaseSum / 100))
+}
+
+inputRange1.addEventListener('input', updateValues)
+inputRange2.addEventListener('input', updateValues)
+inputRange3.addEventListener('input', updateValues)
+
+
+
+
